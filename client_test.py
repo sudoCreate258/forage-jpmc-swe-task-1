5be59bab2c0b8f06fae739d6b63ec3d501c23d90
sc258 <sudoCreate258@gmail.com>
2024-03-06 16:40:01 -0500

    introduced simple assertion tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for q in quotes:
+        self.assertEqual(getDataPoint(q), (q['stock'],q['top_bid']['price'],q['top_ask']['price'], (q['top_bid']['price'] + q['top_bid']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +17,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for q in quotes:
+        self.assertEqual(getDataPoint(q), (q['stock'],q['top_bid']['price'],q['top_ask']['price'], (q['top_bid']['price'] + q['top_bid']['price'])/2))
 
-
-  """ ------------ Add more unit tests ------------ """
-
-
+    """ ------------ Add more unit tests ------------ """
 
 if __name__ == '__main__':
     unittest.main()
